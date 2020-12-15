20ee7905ad7f40e9612981f4a498e1aa39e7122e
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-12-15 10:43:11 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <h1>This is the main app of the restaurant</h1>
+    </div>
+
+  );
+}
+
+export default App;
